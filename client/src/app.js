25c92a5b402f1fcd6ef41f5002ce5bbd4577980c
framista <adajab1@gmail.com>
2020-02-05 01:03:40 +0100

    Modify chart

--- client/src/app.js
+++ client/src/app.js
@@ -1,24 +1,60 @@
 import api from '../api'
-var Chart = require('chart.js');
 
-var ctx = document.getElementById('myChart');
+const dateIpt = document.getElementById("date")
+dateIpt.value = todayDate();
 
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Temperatures',
-            backgroundColor: 'rgb(255, 255, 255, 0.1)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-});
+const Chart = require('chart.js');
+const ctx = document.getElementById('myChart');
+
+api.getTemperatures("2020-01-01").then(result => {
+
+    const measurements = result.data
+    const dataChart = measurements.map(measurement => {
+        const { temp1, temp2, temp3, temp4, date } = measurement;
+        let meanTemperature = (temp1 + temp2 + temp3 + temp4) / 4;
+        return {
+            x: new Date(date).toUTCString(),
+            y: meanTemperature
+        }
+    })
+    console.log(dataChart)
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Temperatures',
+                backgroundColor: 'rgb(255, 255, 255, 0.1)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: dataChart
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            unit: 'hour'
+                        }
+                    }
+                }]
+            }
+        }
+    });
+
+})
+
+function todayDate() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
 
 
-
-
+/*
 api.getMeasurementById(5).then(measurement => console.log(measurement.data))
 
 const measurement = {
@@ -32,3 +68,5 @@ api.insertMeasurement(measurement).then(result => console.log(result.data))
 
 const date = { "date": "2020-11-11" }
 api.getTemperatures("2020-11-11").then(result => console.log(result.data))
+
+*/
